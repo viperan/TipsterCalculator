f84a79a13023883366a3490923de7e37ce4884a0
Daniel Ardiwinata <danielardiwinata@daniels-macbook-pro.local>
2015-12-16 15:39:28 -0800

    Initial Commit

---
+++ UserDefaultsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefaultsExample
+//
+//  Created by Daniel Ardiwinata on 12/16/15.
+//  Copyright © 2015 Daniel Ardiwinata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
